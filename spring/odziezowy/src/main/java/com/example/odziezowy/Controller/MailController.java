bd0b66ea5acae82ebe2a2ec86e3e409ae5142680
tytu1233 <patryk1233tytu@gmail.com>
2022-11-11 16:46:05 +0100

    mail configuration

---
+++ spring/odziezowy/src/main/java/com/example/odziezowy/Controller/MailController.java
@@ -0,0 +1,26 @@
+package com.example.odziezowy.Controller;
+
+import com.example.odziezowy.Service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+
+@RestController
+public class MailController {
+
+    private final MailService mailService;
+
+    @Autowired
+    public MailController(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    @GetMapping("/sendMail")
+    public String sendMail() throws MessagingException {
+        mailService.sendMail();
+        return "wysłano";
+    }
+
+}
---
+++ spring/odziezowy/src/main/java/com/example/odziezowy/Service/MailService.java
@@ -0,0 +1,35 @@
+package com.example.odziezowy.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class MailService {
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    public MailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendMail() throws MessagingException {
+        String from = "sklepodziezowynaix@gmail.com";
+        String to = "letsgetitingirona@gmail.com";
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject("This is a plain text email");
+        message.setText("Hello guys! This is a plain text email.");
+
+        javaMailSender.send(message);
+    }
+
+}
